3cb9945e4204136e9391196ef9b110bf31675428
Octavio Duarte <octavioduarte2105@gmail.com>
2021-04-29 15:23:17 -0300

    feat: create enunciated of question 01 of class 01

---
+++ src/class01/01-var-and-const.js
@@ -0,0 +1,14 @@
+// Class 01
+
+// Create a new branch and perform the following tasks:
+
+
+// Use variables or constants to write 10 data about you
+
+// Tip: Use as a criterion to choose between variable and constant 
+// information that is or is not mutable in you
+
+// Then make a Pull Request for the master branch of this repository
+
+
+
