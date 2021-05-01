4aa60ce7e1cb8c59f3b17dd2b8585b53eb9a2784
Octavio Duarte <octavioduarte2105@gmail.com>
2021-05-01 20:40:44 -0300

    test: create test to CHECK IF (primitiveTypes) HAS CORRECT LENGTH - 02 primitive types

---
+++ tests/class01/02-primitive-type-correct-length.test.js
@@ -0,0 +1,35 @@
+import { primitiveTypes } from '../../src/class01/02-primitive-types'
+import { handleLogTest } from '../../utils/handle-log'
+import { types, lesson } from './util'
+
+
+
+(() => {
+    let logProps = {
+        lesson,
+        testName: 'CHECK IF (primitiveTypes) HAS CORRECT LENGTH',
+        testSuccess: true,
+        tip: ""
+    }
+
+    const correctLength = types.length
+    const primitiveTypesLength = primitiveTypes.length
+
+
+    if (primitiveTypesLength !== correctLength) {
+        logProps = {
+            ...logProps,
+            testSuccess: false,
+            tip: `YOU REPORTED ${primitiveTypesLength} - EXPECTED ${correctLength}`
+        }
+    } else {
+        logProps = {
+            ...logProps,
+            testSuccess: true,
+        }
+    }
+
+
+
+    handleLogTest(logProps)
+})()
