01b1ed75e3208bc1b35e34bad5a14635eb3684fb
Christian404 <gchristian.oliveira@gmail.com>
2021-06-19 18:08:32 -0300

    feat: describe my personal data with var or const

--- src/class01/01-var-and-const.js
+++ src/class01/01-var-and-const.js
@@ -10,5 +10,14 @@
 
 // Then make a Pull Request for the master branch of this repository
 
-
+const name = "gabriel"
+var age = 20
+var hobby = "games"
+var preferredColorBlack = true
+var studiedLinguage = "java script"
+const favoriteMusicStyle = "rock"
+const dogName = "babu"
+var dogAge = 4
+var favoriteGame = "halo"
+var intendedPosition = "java script developer"
 
