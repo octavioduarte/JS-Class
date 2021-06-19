a3c7ffa20b29bead0894032ca478cfdd2388faee
octavioduarte <octavioduarte2105@gmail.com>
2021-06-19 17:27:52 -0300

    feat: create instructions about name of data

---
+++ src/tips/names-of-data.js
@@ -0,0 +1,26 @@
+
+
+
+// File names use skewer-case
+
+// Examples
+
+const fileName = "file-name.js"
+const fileName2 = "file-name-2.js"
+
+
+// vars, const or function names  use camelCase 
+
+// Examples
+
+var fatherName = "Robson Duarte"
+const addressRemote = "octavioduarte2105@gmail.com"
+
+
+// Class name use PascalCase
+
+// Examples 
+
+class PersonData {
+
+}
