8f33013e729dc7320c3cc2ca9281d6b2cbe016bb
Christian404 <gchristian.oliveira@gmail.com>
2021-06-27 01:16:23 -0300

    feat:brief description of javascript primitive types

--- src/class02/01-primitive-types.js
+++ src/class02/01-primitive-types.js
@@ -2,6 +2,23 @@ export const primitiveTypes =
     [
         // After this row
 
+        "String",
+        // Em qualquer linguagem de programação, uma string é uma sequência de caracteres usados para representar texto
+        
+        "Number",
+        // Number é um tipo de dado numérico
+        
+        "Booleano",
+        //Um booleano, em ciência da computação, é um tipo de dado lógico que pode ter apenas um de dois valores possíveis: verdadeiro ou falso. Por exemplo, em JavaScript, condicionais booleanas são usadas para decidir quais trechos do código serão executados ou repetidas.
+        
+        "Nulo",
+        // Em ciência da computação, um valor nulo representa uma referencia que aponta, geralmente de maneira intencional, para um objeto ou endereço de memória inválido ou inexistente. O significado do valor nulo varia entre as implementações das linguagens.
+        
+        "undefined",
+        // Um valor  primitive automaticamente atribuido para variaveis que foram recentemente declaradas ou para arguments formais para qual não existem argumentos atualmente.
+        
+        "Symbo",
+        //Esse termo do glossário descreve tanto o tipo de dados chamado "symbol", quando a função/classe "Symbol()", que entre outras coisas, cria instâncias do tipo de dados symbol.
 
         // Before this row
     ]
