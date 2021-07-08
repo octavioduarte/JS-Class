fb8499d8d1be55bd174e868fcbcac15fb6b144b6
octavioduarte <octavioduarte2105@gmail.com>
2021-07-08 11:41:31 -0300

    refactor: change name of vars

--- src/class04/ascending_order.js
+++ src/class04/ascending_order.js
@@ -1,29 +1,29 @@
-var x = 40
-var y = 20
-var z = 10
+/*
+ Write a program to read 3 integer values ​​(assume they will not be equal values) 
+ and write them in  ascending order. in JS 
+*/
 
 
-if ((x > y) && (x < z)){
+var v1 = 3
+var v2 = 50
+var v3 = 30
 
-    console.log (y,x,z)
 
-}else if ((y > x) && (y < z)){
+if ((v1 > v2) && (v1 < v3)) {
+    console.log(v2, v1, v3)
 
-    console.log (x,y,z)
+} else if ((v2 > v1) && (v2 < v3)) {
+    console.log(v1, v2, v3)
 
-}else if ((z > x) && (z < y))
-{
-    console.log (x,z,y)
+} else if ((v3 > v1) && (v3 < v2)) {
+    console.log(v1, v3, v2)
 
-}else if ((x > z) && (x < y)){
+} else if ((v1 > v3) && (v1 < v2)) {
+    console.log(v3, v1, v2)
 
-    console.log (z,x,y)
+} else if ((v2 > v3) && (v2 < v1)) {
+    console.log(v3, v2, v1)
 
-}else if ((y > z) && (y < x)){
-
-    console.log (z,y,x)
-
-}else if ((z > y) && (z < x))
-{
-    console.log (y,z,x)
+} else  {
+    console.log(v2, v3, v1)
 }
--- src/class04/higher_value.js
+++ src/class04/higher_value.js
@@ -1,18 +1,22 @@
-var x = 40
-var y = 50
-var z = 100
+/*
+    Write a program to read 3 integer values ​​(assuming the values ​​will not be equal)
+     and print the largest value between them.
+*/
 
+var v1 = 40
+var v2 = 40
+var v3 = 40
 
-if ((x > y) && (x > z)){
+if ((v1 > v2) && (v1 > v3)){
 
-    console.log ("The X value is the largest")
+    console.log ("The v1 value is the largest")
+}else if ((v2 > v1) && (v2 > v3)){
 
-}else if ((y > x) && (y > z)){
-
-    console.log ("The Y value is the largest")
-
-}else if ((z > x) && (z > y))
+    console.log ("The v2 value is the largest")
+}else if ((v3 > v1) && (v3 > v2))
 {
-    console.log ("The Z value is the largest")
+    console.log ("The v3 value is the largest")
+} else {
 
+    console.log("Vars with same values")
 }
