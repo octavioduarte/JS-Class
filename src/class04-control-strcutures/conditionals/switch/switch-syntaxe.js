3b4a92fb1f6137e0566df3c8eff89c68be75b02c
octavioduarte <octavioduarte2105@gmail.com>
2021-07-08 13:52:23 -0300

    feat: tutorial about syntaxe of switch

---
+++ src/class04-control-strcutures/conditionals/switch/switch-syntaxe.js
@@ -0,0 +1,28 @@
+const dayWeek = "Quinta"
+
+
+switch (dayWeek) {
+    case "Segunda":
+        console.log("Hoje é um péssimo dia")
+        break;
+    case "Terça":
+        console.log("Hojé é o 2º pior dia")
+        break;
+    case "Quarta":
+        console.log("Hoje é dia de jogo na TV")
+        break;
+    case "Quinta":
+        console.log("Falta só 1 dia")
+        break;
+    case "Sexta":
+        console.log("Dia de tomar uma")
+        break;
+    case "Sabádo":
+        console.log("Hoje é dia de tomar umas dnv !!! ")
+        break;
+    case "Domingo":
+        console.log("Hoje é dia de Faustão")
+        break;
+    default:
+        console.log("Dia inválido")
+}
