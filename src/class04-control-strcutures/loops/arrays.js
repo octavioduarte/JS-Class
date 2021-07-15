42c9316a92d67e651d9f18652a6637b28b64a762
octavioduarte <octavioduarte2105@gmail.com>
2021-07-15 11:57:12 -0300

    feat: tutorial about arrays

---
+++ src/class04-control-strcutures/loops/arrays.js
@@ -0,0 +1,18 @@
+/*
+    Arrays are ways to store values ​​within a single variable.
+*/
+
+// Without array 
+
+const fruit1  = "Banana"
+const fruit2  = "Apple"
+const fruit3  = "Pineapple"
+
+
+// With Arrays 
+const fruits = ["Banana", "Apple", "Pineapple"]
+
+
+// Values ​​within an array are numeric indices, where the first value has index 0
+
+console.log(fruits[0]) // >> "Banana"
