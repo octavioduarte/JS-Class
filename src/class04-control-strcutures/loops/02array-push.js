08cad245cdb79d53be75ec59594fd58a4360d896
octavioduarte <octavioduarte2105@gmail.com>
2021-07-15 17:43:13 -0300

    feat: documentation about push method

--- src/class04-control-strcutures/loops/02array-push.js
+++ src/class04-control-strcutures/loops/02array-push.js
@@ -1,3 +1,8 @@
+/*
+    Appends new elements to the end of an array, and returns the
+    new length of the array.
+*/
+
 const bands = ["Black Sabbath", "AC/DC", "Deep Purple"]
 
 console.log("Before push: ", bands)
