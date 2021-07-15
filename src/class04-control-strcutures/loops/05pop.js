9fea79e7d95748571761170ac35e44d0902d3aa6
octavioduarte <octavioduarte2105@gmail.com>
2021-07-15 18:16:29 -0300

    feat: tutorial about array.pop

---
+++ src/class04-control-strcutures/loops/05pop.js
@@ -0,0 +1,9 @@
+/*
+     Removes the last element from an array and returns it.
+     If the array is empty, undefined is returned and the array is not modified.
+ */
+const people = ["Janis Joplin", "Jimi Hendrix", "Robert Jhonson", "Ozzy Osbourne"]
+
+people.pop()
+
+console.log(people) // > [ 'Janis Joplin', 'Jimi Hendrix', 'Robert Jhonson' ]
