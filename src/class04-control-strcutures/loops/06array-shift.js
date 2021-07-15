1de44962ab4f400a43feac267bc6f6653d942df0
octavioduarte <octavioduarte2105@gmail.com>
2021-07-15 18:20:55 -0300

    feat: tutorial about array.shift

---
+++ src/class04-control-strcutures/loops/06array-shift.js
@@ -0,0 +1,10 @@
+/*
+    Removes the first element from an array and returns it. If
+    the array is empty, undefined is returned and the array is not modified.
+*/
+
+const pairNumbers = [9, 10, 22, 18, 40]
+
+pairNumbers.shift()
+
+console.log(pairNumbers) // > [10, 22, 18, 40]
