b404f86c7a429f0af79285ac6bd3fe4a159ac19f
octavioduarte <octavioduarte2105@gmail.com>
2021-07-15 12:30:20 -0300

    feat: tutorial about array.push

---
+++ src/class04-control-strcutures/loops/array-push.js
@@ -0,0 +1,11 @@
+const bands = ["Black Sabbath", "AC/DC", "Deep Purple"]
+
+console.log("Before push: ", bands)
+
+bands.push("Metallica", "Led Zeppelin")
+
+console.log("After push: ", bands)
+
+
+
+
