f768b030e9c4e43639049a068d393387332ad682
octavioduarte <octavioduarte2105@gmail.com>
2021-07-15 17:43:44 -0300

    feat: tutorial about unshift

---
+++ src/class04-control-strcutures/loops/03unshift.js
@@ -0,0 +1,10 @@
+/*
+    Inserts new elements at the start of an array, and returns 
+    the new length of the array.
+*/
+
+const colors = ["Black", "White"]
+
+colors.unshift("Red")
+
+console.log(colors)
