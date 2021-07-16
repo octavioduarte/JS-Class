1214f1d46f7728c04606ffdad875b6dbf84f303a
octavioduarte <octavioduarte2105@gmail.com>
2021-07-16 11:49:14 -0300

    feat: exercise about for inside for

---
+++ src/class04-control-strcutures/loops/list-client.js
@@ -0,0 +1,39 @@
+const clientsBrazil = [
+    'hdkjakmwd5n6dvjfscua98@uk.io',
+    '9xik7hlwqcnjrr24ezgjog@brazil.com.br',
+    'ykccd8f1g3n6i5vujyoog9@uk.io',
+    'c08edjvzwgasg7rlj2bb1j@brazil.com.br',
+    'nvglhwtvf2ltlhna28yvq@uk.io',
+    'oxe2yjy7obbjzfh864rxd@brazil.com.br',
+    'ybyfetixyma5fmrmyotm@eua.com',
+    'nsg3tkklwus5liwj8o4iib@eua.com',
+    'alof4a50819atxklkylwg@brazil.com.br',
+    'wfrdgjdvnielz1cj22mqvc@uk.io',
+]
+
+const clientsEua = [
+    'nvgawvcra7ci6qz3oa0rn@brazil.com.br',
+    'g259klm0b2hees5wzbboa@uk.io',
+    'man67pz59j3tvkrf3kjmf@uk.io',
+    'veuspzz62eozf3s1j6wl1@brazil.com.br',
+    'vlazo0u37vq9ru0kq71jtv@uk.io',
+    'nk8loo337tjoqum9wfd5y9@eua.com',
+    '1zrkcrhfap933kdqfww5n@brazil.com.br',
+    'pxk6fdt2s45fu6nvl306@eua.com'
+]
+
+const clientsUK = [
+    '6puxauwvt9otb27l115zlt@uk.io',
+    '7mtdy99ylbw7mwcj39lib6@brazil.com.br',
+    '9utvus7oxekv7dqix592ks@eua.com',
+    'd24r2e0imsibjk1siolq@uk.io',
+    'sl18okldoon661zny5fbah@eua.com',
+    '6pkamyn79jfig4ejoonvh7@brazil.com.br',
+    'ywwvmh28xfm8yxplylzt3g@eua.com',
+    'q8dy8szx8yglelan1697p@eua.com',
+    'y8sdyejw0ejkn30bucg4@brazil.com.br',
+    'l351bxnhism7khpxr8ar96@eua.com',
+    'efcaq2rq3us6u73f40e66@uk.io',
+    '3490lqjrx3ck2ds64y83tn@eua.com'
+]
+
