1eebe1303f3f9e5358aebb9dba907c5cc1b9ee24
octavioduarte <octavioduarte2105@gmail.com>
2021-07-23 01:42:43 -0300

    feat: revision about for structure

---
+++ src/class04-control-strcutures/loops/09for-revision.js
@@ -0,0 +1,99 @@
+/**
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+ * O for executa o mesmo trecho de código por finitas vezes
+*/
+
+// const name = "Octávio"
+// const name2 = "ana"
+// const name3 = "Pedro"
+// const name4 = "gabriel";
+
+// const names = [
+//     "Octávio",
+//     "ana",
+//     "Pedro",
+//     "gabriel",
+//     "Eduardo"
+// ]
+
+// if (name.verificaSePrimeiraLetraÉMinuscula() === true) {
+//     console.log("Nome incorreto")
+// } else {
+//     console.log("Nome válido")
+// }
+
+// if (name2.verificaSePrimeiraLetraÉMinuscula() === true) {
+//     console.log("Nome incorreto")
+// } else {
+//     console.log("Nome válido")
+// }
+
+// if (name3.verificaSePrimeiraLetraÉMinuscula() === true) {
+//     console.log("Nome incorreto")
+// } else {
+//     console.log("Nome válido")
+// }
+
+// if (name4.verificaSePrimeiraLetraÉMinuscula() === true) {
+//     console.log("Nome incorreto")
+// } else {
+//     console.log("Nome válido")
+// }
+
+
+// for (var i = 0; i < names.length; i++) {
+//     if (name.verificaSePrimeiraLetraÉMinuscula() === true) {
+//         console.log("Nome incorreto")
+//     } else {
+//         console.log("Nome válido")
+//     }
+// }
+
+
+//------------------------------------------------------------------------------------------------------
+
+const numero = 84
+
+
+for (var i = numero; i <= numero * 10; i = i + numero) {
+    console.log(">>>", i)
+}
