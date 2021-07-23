0d101dfd5e2afd1ebc52692248d323cb44202581
octavioduarte <octavioduarte2105@gmail.com>
2021-07-23 00:48:42 -0300

    feat: instructions about for and arrays

---
+++ src/class04-control-strcutures/loops/08for-and-arrays.js
@@ -0,0 +1,33 @@
+// For e Arrays são coisas diferentes
+
+
+// For --> Uma estrutura de repetição que executa um trecho de código por um número finito de vezes
+
+for (var i = 0; i <= 100; i++) {
+    console.log("Valor de i >>>", i)
+}
+
+// ------------------------------------------------------------------------------------------------------
+
+// Arrays --> Uma forma de agruparmos diversos dados em unico local 
+
+// Dados soltos
+const name = "Gabriel"
+const name2 = "Octávio"
+
+// Com Arrays
+const myArray = ["Octávio", "Gabriel"]
+
+// ------------------------------------------------------------------------------------------------------
+
+// Usando for com array 
+
+const bands = ["AC/DC", "Metallica", "Slayer", "Kiss", "Black Sabbath"]
+
+
+for (var i = 0; i < bands.length; i++) {
+    console.log(">>", bands[i])
+}
+
+
+// ------------------------------------------------------------------------------------------------------
